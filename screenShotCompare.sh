abdfdae189a9123eeb1d7185bfa119556863fb2d
Hannes Achleitner <hannes.software@gmx.at>
2024-12-01 10:16:35 +0100

    Cosmetic

--- screenShotCompare.sh
+++ screenShotCompare.sh
@@ -2,7 +2,6 @@
 
 diffFiles=./screenshotDiffs
 mkdir $diffFiles
-#cp MPChartExample/build/outputs/connected_android_test_additional_output/debugAndroidTest/connected/emulator\(AVD\)\ -\ 9/* screenshotsToCompare
 set -x
 ./git-diff-image/install.sh
 GIT_DIFF_IMAGE_OUTPUT_DIR=$diffFiles git diff-image
